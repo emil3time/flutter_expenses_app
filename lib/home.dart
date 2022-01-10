5ea4e29b26fe77f85bd2e97d31dcfbc18fb75162
Emil <ezmil34@gmail.com>
2022-01-10 15:16:00 +0100

    add intl package and format time,add text fields

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'expensesEvent.dart';
 
 class HomeApp extends StatelessWidget {
@@ -23,75 +24,104 @@ class HomeApp extends StatelessWidget {
     ),
   ];
 
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController priceController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.amber,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              padding: EdgeInsets.all(10),
-              width: double.infinity,
-              color: Colors.blue,
-              child: Text('Week Statistic'),
-            ),
-            Column(
-              children: [
-                ...events.map(
-                  (event) => Card(
-                    elevation: 10,
-                    child: Row(
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 10, 10, 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Card(
+                color: Colors.blue,
+                child: Text('Week Statistic'),
+              ),
+              Card(
+                elevation: 10,
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(labelText: 'expenses name'),
+                    ),
+                    TextField(
+                      controller: priceController,
+                      decoration: InputDecoration(labelText: 'price (PLN)'),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: [
-                        Container(
-                          width: 60,
-                          margin: EdgeInsets.all(3),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Color(0xffadaddd),
-                            ),
-                          ),
-                          padding: EdgeInsets.all(10),
-                          child: Text(
-                            event.price.toString(),
-                            textAlign: TextAlign.end,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 30,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              event.name,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 24,
-                              ),
-                            ),
-                            Text(
-                              event.time.toString(),
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
-                                  color: Colors.grey),
-                            ),
-                          ],
-                        )
+                        ElevatedButton(
+                            onPressed: () {
+                              print(priceController.text);
+                            },
+                            child: Text('add new event')),
                       ],
                     ),
-                  ),
-                )
-              ],
-            )
-          ],
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  ...events.map(
+                    (event) => Card(
+                      elevation: 15,
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 60,
+                            margin: EdgeInsets.all(3),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.blue,
+                                width: 2,
+                              ),
+                            ),
+                            padding: EdgeInsets.all(10),
+                            child: Text(
+                              event.price.toString(),
+                              textAlign: TextAlign.end,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 30,
+                                  color: Colors.blue),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                event.name,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24,
+                                ),
+                              ),
+                              Text(
+                                DateFormat.yMMMMEEEEd().format(event.time),
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                    color: Colors.grey),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
